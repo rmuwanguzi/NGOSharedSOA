3c11148e696aa60a26d857b375cac85f59ea8c30
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-23 00:52:39 +0300

    add ILoggedInUserService.cs

---
+++ Voice.Shared/Interfaces/ILoggedInUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Voice.Shared.Core.dto;
+
+namespace Voice.Shared.Core.Interfaces
+{
+   public interface ILoggedInUserService : IService
+    {
+        dto_logged_user GetLoggedInUser();
+
+    }
+}
