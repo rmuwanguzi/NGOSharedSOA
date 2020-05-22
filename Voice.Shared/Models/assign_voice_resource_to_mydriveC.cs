83a38592ebd02a87946e2926fab31b376b9c2469
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-23 00:54:03 +0300

    add assign_voice_resource_to_mydriveC.cs

---
+++ Voice.Shared/Models/assign_voice_resource_to_mydriveC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voice.Shared.Core.Models
+{
+    public class assign_voice_resource_to_mydriveC : serverC
+    {
+        public int user_id { get; set; }
+        public int resource_id { get; set; }
+        public int un_id { get; set; }
+    }
+}
