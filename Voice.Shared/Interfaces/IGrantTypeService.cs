ba9afa0a8e97e2f3160bb79fe1531f13250308eb
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-23 00:51:54 +0300

    add IGrantTypeService.cs

---
+++ Voice.Shared/Interfaces/IGrantTypeService.cs
@@ -0,0 +1,15 @@
+﻿using Voice.Shared.Core.dto;
+using Voice.Shared.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voice.Shared.Core.Interfaces
+{
+    public interface IGrantTypeService : IService, IServiceCore<grant_typeC, dto_grant_type_updateC>
+    {
+        Task<grant_typeC> AddNew(dto_grant_type_newC _dto);
+    }
+}
