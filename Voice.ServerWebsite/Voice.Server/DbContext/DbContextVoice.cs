0c02e30d7a656c17cde4f206367a23afe5e2b215
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-22 15:39:57 +0300

    add DbContext/

---
+++ Voice.ServerWebsite/Voice.Server/DbContext/DbContextVoice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Voice.DbBase;
+
+namespace Voice.Server
+{
+    public class DbContextVoice : BaseContext
+    {
+        public DbContextVoice() : base(new SqlConnection(fn.CONN_STRING), true, "voice_dev2")
+        {
+
+        }
+    }
+}
