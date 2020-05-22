60710b7d97f017c95f14d2287c296625e61b5e5d
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-23 00:47:46 +0300

    add IKellerDBContext.cs

---
+++ Voice.Services/Voice.Services/Interfaces/IKellerDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voice.Service.Interfaces
+{
+    public interface IKellerDBContext
+    {
+        KellermanSoftware.NetDataAccessLayer.DataHelper GetContext();
+    }
+}
