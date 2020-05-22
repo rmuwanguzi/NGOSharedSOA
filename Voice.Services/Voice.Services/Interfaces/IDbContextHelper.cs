d35306a0a16ae528c8ec22c6c6a7a4c90bca9a50
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-23 00:47:39 +0300

    add IDbContextHelper.cs

---
+++ Voice.Services/Voice.Services/Interfaces/IDbContextHelper.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Data.Common;
+using System.Data.Entity.Infrastructure;
+
+namespace Voice.Service.Interfaces
+{
+   public interface IDbContextHelper
+    {
+        DbBase.BaseContext GetContext();
+        DbParameter GetDbParameters(string parameterName, object value);
+        bool? IsSaveDuplicateRecordError(DbUpdateException ex);
+        bool? IsUpdateDuplicateRecordError(DbException ex);
+        DbConnection GetDBConnection();
+    }
+}
