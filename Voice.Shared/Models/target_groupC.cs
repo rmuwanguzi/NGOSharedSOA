49b2f92cc5929779cf78a9c2a2df77b0a4f7b082
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-23 00:54:42 +0300

    add target_groupC.cs

---
+++ Voice.Shared/Models/target_groupC.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Voice.Shared.Core.Models
+{
+    public class target_groupC : serverC
+    {
+        public int target_gp_id { get; set; }
+        public string target_gp_name { get; set; }
+        public int no_of_grantees { get; set; }
+        public string target_gp_image_url { get; set; }
+        public int no_of_users { get; set; }
+        public string alias { get; set; }
+        public int media_count { get; set; }
+        public int publication_count { get; set; }
+        public int story_count { get; set; }
+    }
+}
