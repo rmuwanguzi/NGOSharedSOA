92f5d8942e6dc2a4dcc6b835c9df9f8679c1d61c
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-23 00:46:53 +0300

    add DbHelperDeleteRecordC.cs

---
+++ Voice.Services/Voice.Services/Helpers/DbHelperDeleteRecordC.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Voice.Service
+{
+    internal class DbHelperDeleteRecordC
+    {
+        public string table_name { get; set; }
+        public string pk_col_name { get; set; }
+        public int pk_id { get; set; }
+    }
+}
