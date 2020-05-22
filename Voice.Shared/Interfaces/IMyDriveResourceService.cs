cdaef4f728a6a127d5f6155f399aa53e92cb26b4
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-23 00:52:04 +0300

    add IMyDriveResourceService.cs

---
+++ Voice.Shared/Interfaces/IMyDriveResourceService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Voice.Shared.Core.dto;
+using Voice.Shared.Core.Models;
+
+namespace Voice.Shared.Core.Interfaces
+{
+    public interface IMyDriveResourceService : IService
+    {
+        Task<bool> AddResourceToMyDrive(int resource_id);
+        Task<List<voice_resourceC>> GetAllMyDriveResourcesByUserId();
+        Task<bool> RemoveResourceFromMyDrive(int resource_id);
+
+    }
+}
