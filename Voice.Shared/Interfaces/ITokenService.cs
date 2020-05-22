d67df2361def7036cb02d01abe57af7ac074b983
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-23 00:52:25 +0300

    add ITokenService.cs

---
+++ Voice.Shared/Interfaces/ITokenService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Voice.Shared.Core.Models;
+
+namespace Voice.Shared.Core.Interfaces
+{
+   public  interface ITokenService : IService
+    {
+        string GenerateToken(voice_userC _obj, double duration_minutes);
+        ClaimsPrincipal ValidateToken(string access_token);
+    }
+}
