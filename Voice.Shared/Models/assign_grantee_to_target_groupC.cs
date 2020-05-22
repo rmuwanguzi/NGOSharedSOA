23518ee09da6aaed9780f74978f384bd83954a26
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-23 00:53:39 +0300

    add assign_grantee_to_target_groupC.cs

---
+++ Voice.Shared/Models/assign_grantee_to_target_groupC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voice.Shared.Core.Models
+{
+    public class assign_grantee_to_target_groupC : serverC
+    {
+        public int grantee_id { get; set; }
+        public int target_gp_id { get; set; }
+        public int grant_type_id { get; set; }
+        public int un_id { get; set; }
+    }
+}
