8b6b06c2bbbc68007c22676d14c0ca02f49786fe
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-23 00:46:46 +0300

    add DataExtensions.cs

---
+++ Voice.Services/Voice.Services/Helpers/DataExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Voice.Service
+{
+    internal static class DataExtensions
+    {
+        public static IEnumerable<T> ToFullyLoaded<T>(this IQueryable<T> query)
+        {
+            return query.ToArray().ToList();
+        }
+        public static IEnumerable<T> ToFullyLoaded<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable.ToList();
+        }
+    }
+}
