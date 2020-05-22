51e76b5bafc4908fa7b0518674dfe4028ba0d12e
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-23 00:52:12 +0300

    add IMessageDialog.cs

---
+++ Voice.Shared/Interfaces/IMessageDialog.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Voice.Shared.Core.Interfaces
+{
+    public interface IMessageDialog
+    {
+    
+        void ErrorMessage(string message, string title = null);
+        void ErrorMessage(string key, string message, string controller_key=null);
+         
+        
+    }
+}
