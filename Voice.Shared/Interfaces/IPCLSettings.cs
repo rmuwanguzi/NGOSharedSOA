32a2982ce91061dfcc6a59b9bac50d4f23f59f61
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-23 00:52:18 +0300

    add IPCLSettings.cs

---
+++ Voice.Shared/Interfaces/IPCLSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voice.Shared.Core.Interfaces
+{
+   public interface IPCLSettings
+    {
+        string BaseUrl { get; set; }
+    }
+}
