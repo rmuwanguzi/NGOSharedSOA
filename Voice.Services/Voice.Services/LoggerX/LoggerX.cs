3a0eb4ba433fdada3b07de39372ec7ca1794dc11
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-23 00:48:04 +0300

    add LoggerX.cs

---
+++ Voice.Services/Voice.Services/LoggerX/LoggerX.cs
@@ -0,0 +1,46 @@
+﻿using KellermanSoftware.NetLoggingLibrary;
+using KellermanSoftware.NetLoggingLibrary.Targets;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+
+namespace Voice.Service.LoggerX
+{
+   internal class LoggerX
+    {
+        private static bool is_init = false;
+        private static void initLogger()
+        {
+            if (is_init) { return; }
+            var rootPath = HostingEnvironment.MapPath("~/ErrorLogs/");
+            if(string.IsNullOrEmpty(rootPath))
+            {
+                is_init = false;
+                return;
+            }
+            string _file_name = "errorLog.txt";
+            var _errorFilePath = Path.Combine(rootPath, Path.GetFileName(_file_name));
+            FileTarget errorTarget = new FileTarget(_errorFilePath);
+            errorTarget.MinimumLevel = LoggingLevel.ERROR;
+            Log.Config.Targets.Add(errorTarget);
+           
+            is_init = true;
+        }
+        
+        public static void LogException(System.Exception ex)
+        {
+            initLogger();
+            System.Diagnostics.Debug.WriteLine("<<ERROR>>");
+            System.Diagnostics.Debug.WriteLine(ex.Message);
+            if (is_init)
+            {
+                Log.LogException(ex);
+            }
+            
+        }
+    }
+}
